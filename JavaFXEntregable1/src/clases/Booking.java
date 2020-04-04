bef44bca6c6a211f6095ad9c10de2ac8b4346f34
Dorian <aarenast15@gmail.com>
2020-04-04 12:53:21 +0200

    Clases

---
+++ JavaFXEntregable1/src/clases/Booking.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author aaren
+ */
+public class Booking {
+    
+    private LocalDateTime bookingDate;
+    private LocalDate madeForDay;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ JavaFXEntregable1/src/clases/Club.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aaren
+ */
+public class Club {
+    
+    
+    private String name;
+    private final int bookingDuration=90;
+    private final int bookingSlots = 9;
+    
+    ArrayList<Court> courts;
+    ArrayList<Member> members;
+    ArrayList<Booking> bookings;
+    
+    
+    
+}
